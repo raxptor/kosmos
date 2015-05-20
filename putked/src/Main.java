f24600beab4c93926e202f989fc740f8860dab78
Dan Nilsson <raxptor@gmail.com>
2015-05-20 10:01:53 +0200

    - java

--- putked/src/Main.java
+++ putked/src/Main.java
@@ -1,18 +1,27 @@
+import inki.Kosmos;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import putked.*;
 
 public class Main extends Application 
 {
-    public static void main(String[] args) 
-    {
-       Main.launch(args);
-    }
+	public static void main(String[] args) 
+	{
+		putked.Main.initEditor();
+		initEditor();
+		Main.launch(args);
+	}
+	
+	public static void initEditor()
+	{
+		DataHelper.addTypeService(new Kosmos());
+		putked.Main.addEditor(new TextureEditor());		
+		putked.Main.addImporter(new TextureImporter());
+	}
     
     @Override
     public void start(Stage stage) 
     {
-    	inki.Kosmos.register();
     	putked.Main m = new putked.Main();
     	m.start(stage);
     }
