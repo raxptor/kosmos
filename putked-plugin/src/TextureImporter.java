6e3f4a72602e4b9464ba05aaed75ac74726298d2
Dan Nilsson <raxptor@gmail.com>
2015-06-03 11:01:20 +0200

    - Added JavaFX plugin - Removed csharp putked

---
+++ putked-plugin/src/TextureImporter.java
@@ -0,0 +1,56 @@
+import inki.Kosmos;
+
+import java.io.*;
+
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import putked.DataHelper;
+import putked.DataImporter;
+import putked.Interop;
+
+public class TextureImporter implements DataImporter 
+{
+	public String getName()
+	{
+		return "Texture";
+	}
+	
+	public boolean importTo(String path)
+	{
+		System.out.println("Importing to [" + path +"]");
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		fileChooser.getExtensionFilters().addAll(new ExtensionFilter("PNG Files", "*.png"));
+		File selectedFile = fileChooser.showOpenDialog(null);
+		if (selectedFile != null) {
+
+			putked.Main.ImportFinalizationQuestion q = new putked.Main.ImportFinalizationQuestion();
+			String name = selectedFile.getName();
+			int p = name.indexOf('.');
+			if (p >= 0)
+				q.proposedPath = path + name.substring(0, p);
+			else
+				q.proposedPath = path + name;
+			
+			q.proposedResPath = path + name;
+			putked.Main.s_instance.askImportFinalization(q, null);
+			
+			if (q.accepted)
+			{
+				if (false) // TODO: Fix && Interop.writeResFile(selectedFile, q.proposedResPath))
+				{
+					Interop.Type t = Kosmos.Texture._getType();
+					Kosmos.Texture tex = (Kosmos.Texture) DataHelper.createPutkEdObj(t.createInstance(q.proposedPath));
+					tex.setSource(q.proposedResPath);
+					tex.m_mi.diskSave();
+					System.out.println("Import successful");
+					putked.Main.s_instance.startEditing(tex.m_mi.getPath());
+					return true;
+				}
+				System.out.println("Selected [" + selectedFile.getAbsolutePath() + "]");
+				System.out.println("(" + q.proposedPath + ") and (" + q.proposedResPath + ")");
+			}
+		}
+		return false;
+	}
+}
