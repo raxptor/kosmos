348879f90fce6b27fe09f89fd42a6bd61233c11b
Dan Nilsson <raxptor@gmail.com>
2015-06-03 15:26:55 +0200

    - some temptation headers

--- src/runtime/kosmos/core/mat4.h
+++ src/runtime/kosmos/core/mat4.h
@@ -2,14 +2,14 @@
 
 namespace kosmos
 {
-	struct vec4f
-	{
-		vec4(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w)
-		{
-
-		};
-
-		float x, y, z, w;
-	};
-
+	// all these are for 16 floats
+	void mat4_identity(float *dest);
+	void mat4_translate(float *dst, float x, float y, float z);
+	void mat4_scale(float *dst, float x, float y, float z);
+	void mat4_rot_x(float *dst, float r);
+	void mat4_rot_y(float *dst, float r);
+	void mat4_rot_z(float *dst, float r);
+	void mat4_mul(float *dst, const float *left, const float *right);
+	void mat4_persp(float *dst, float w, float h, float zn, float zf);
+	void mat4_vec4_mul(float *dst_vec, const float *src_mat, const float *src_vec);
 }
